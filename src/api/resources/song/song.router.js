41e90dbfc91bd04bcaab0316eb52dcd142fa873c
Akande <akande@Akandes-MacBook-Pro.local>
2019-11-03 22:01:20 +0100

    Chnaged listening Port

---
+++ src/api/resources/song/song.router.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import songController from './song.controller';
+
+export const songRouter = express.Router();
+songRouter
+	.route('/')
+	.post(songController.create)
+	.get(songController.findAll);
